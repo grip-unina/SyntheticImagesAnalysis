061414b1c9016d300c8763e4b1721e428bdd0985
RCorvi <41760699+RCorvi@users.noreply.github.com>
2023-07-13 11:29:13 +0200

    Update generate_spectra.py

--- generate_spectra.py
+++ generate_spectra.py
@@ -1,3 +1,14 @@
+#
+# Copyright (c) 2023 Image Processing Research Group of University Federico II of Naples ('GRIP-UNINA').
+# All rights reserved.
+# This work should only be used for nonprofit purposes.
+#
+# By downloading and/or using any of these files, you implicitly agree to all the
+# terms of the license, as specified in the document LICENSE.txt
+# (included in this package) and online at
+# http://www.grip.unina.it/download/LICENSE_OPEN.txt
+#
+
 import matplotlib.pyplot as plt
 from tqdm import tqdm
 import numpy as np
